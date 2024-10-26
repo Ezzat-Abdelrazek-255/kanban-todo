9bbc3be5881a3902299c527d2e9557d2ec86b835
Ezzat Abdelrazek <ezzatabdelrazek255@gmail.com>
2024-10-26 22:51:49 +0300

    refactor(constants): add email field to demo credientials

--- constants/index.ts
+++ constants/index.ts
@@ -1,14 +1,17 @@
 export const DEMO_LOGIN_CREDENTIALS = [
   {
     username: "task-owner-1",
+    email: "taskowner2024@gmail.com",
     password: "taskowner2024",
   },
   {
     username: "task-owner-2",
+    email: "taskowner2023@gmail.com",
     password: "taskowner2024",
   },
   {
     username: "employee",
+    email: "employee2024@gmail.com",
     password: "employee2024",
   },
 ];
