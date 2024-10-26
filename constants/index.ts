0d98d902b9be7633516a745aab5612b01eea5497
Ezzat Abdelrazek <ezzatabdelrazek255@gmail.com>
2024-10-26 16:51:07 +0300

    feat(constants): add demo credientials for testing

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const DEMO_LOGIN_CREDENTIALS = [
+  {
+    username: "task-owner-1",
+    password: "taskowner2024",
+  },
+  {
+    username: "task-owner-2",
+    password: "taskowner2024",
+  },
+  {
+    username: "employee",
+    password: "employee2024",
+  },
+];
