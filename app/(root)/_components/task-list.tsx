215c87209c3d14721ef63088a19b2609c83d990b
Ezzat Abdelrazek <ezzatabdelrazek255@gmail.com>
2024-10-27 03:56:19 +0300

    feat(dashboard): implement html and styling of task-list

---
+++ app/(root)/_components/task-list.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TaskListItem from "./task-list-item";
+
+const TaskList = () => {
+  return (
+    <ul className="mt-6">
+      <TaskListItem />
+    </ul>
+  );
+};
+
+export default TaskList;
