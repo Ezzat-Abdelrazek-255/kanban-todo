c8c2bdb68842d3c88104b85f14f98a8da1c94ae4
Ezzat Abdelrazek <ezzatabdelrazek255@gmail.com>
2024-10-27 23:46:42 +0300

    feat(utils): implement snake to camel case utility function

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,10 @@
 export const capitalize = function(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1);
 };
+
+export const snakeToCamel = function(text: string) {
+  return text
+    .split("_")
+    .map((word, i) => (i === 0 ? word : capitalize(word)))
+    .join("");
+};
