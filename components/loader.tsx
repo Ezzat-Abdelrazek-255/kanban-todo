c3c9f90f7631ff6434943531b5d046e066df10dc
Ezzat Abdelrazek <ezzatabdelrazek255@gmail.com>
2024-10-28 00:25:25 +0300

    refactor(loader): style using tailiwnd instead of css

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,7 +1,15 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
-const Loader = () => {
-  return <span className="loader"></span>;
+const Loader = ({ className }: { className?: string }) => {
+  return (
+    <span
+      className={cn(
+        "w-8 h-8 border-[5px] border-background border-b-transparent rounded-full inline-block animate-spin duration-1000",
+        className,
+      )}
+    ></span>
+  );
 };
 
 export default Loader;
